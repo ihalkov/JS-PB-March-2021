30f7a33dc2f8fadf9624f67f039d73fae3b20312
Ivan <ivan.halkov@gmail.com>
2021-03-07 17:09:01 +0100

    Create learned-first-steps.js

---
+++ learned-first-steps.js
@@ -0,0 +1,13 @@
+// 6 hours, 4 problems
+// theory exam, 15 questions for 30 mins
+    // one or more correct answers
+
+// exam < 4.00, retake exam
+// fundamentals
+// the grades are valid one year after the exam
+    // can be Jan, May, Sept
+    // practical grade (SoftUni Cert + ... continue to next course)
+    // theory grade (CPO Cert)
+    // grade from homework 5%
+
+
